7f969e46fc6af6c1a4a90619ea49ab6c9add602d
ashu540 <87859323+ashu540@users.noreply.github.com>
2021-07-23 18:02:33 +0530

    Add files via upload

---
+++ ashu.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+
+   vector<pair<string,int>>v;
+
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+         
+    }
+   
+
+   return 0;
+
+}
